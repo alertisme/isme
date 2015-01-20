4bb47053ceb1f3e52145d1e6e56d8e82882b642c
isme <alertisme@gmail.com>
2015-01-20 12:26:03 +0800

    commit

--- install_JDK.sh
+++ install_JDK.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+#author: isme
+#time:   2015-1-20
+#默认文件位置: /opt/jdk-7u60-linux-x64.tar.gz
 echo "-------------------install JDK starting --------------"
-chmod 750 /opt/*
+chmod 750 /opt/jdk-7u60-linux-x64.tar.gz
 tar -zxvf /opt/jdk-7u60-linux-x64.tar.gz -C /usr/local/ >>/dev/null
 mv /usr/local/jdk1.7.0_60 /usr/local/java
 echo "" >> /etc/profile
